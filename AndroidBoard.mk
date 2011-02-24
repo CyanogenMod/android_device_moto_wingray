6977e4446bdb9df29a158120e6ffa1de6f2c5cde
Ying Wang <wangying@google.com>
2011-02-24 15:48:52 -0800

    Use PRODUCT_COPY_FILES to install the .rc files.
    So that stingray can override the .rc files with its own.
    
    Change-Id: I985c39af6549eb1e8f1d8741f38a3ad283aa34f2

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -12,27 +12,6 @@ $(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 
 include $(CLEAR_VARS)
 
-target_hw_init_stingray_rc_file := $(TARGET_ROOT_OUT)/init.stingray.rc
-target_hw_init_olympus_rc_file := $(TARGET_ROOT_OUT)/init.olympus.rc
-target_hw_ueventd_stingray_rc_file := $(TARGET_ROOT_OUT)/ueventd.stingray.rc
-target_hw_ueventd_olympus_rc_file := $(TARGET_ROOT_OUT)/ueventd.olympus.rc
-
-$(target_hw_init_stingray_rc_file) : $(LOCAL_PATH)/init.stingray.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-$(target_hw_init_olympus_rc_file) : $(LOCAL_PATH)/init.stingray.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-$(target_hw_ueventd_stingray_rc_file) : $(LOCAL_PATH)/ueventd.stingray.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-$(target_hw_ueventd_olympus_rc_file) : $(LOCAL_PATH)/ueventd.stingray.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-
-ALL_PREBUILT += $(target_hw_init_stingray_rc_file) \
-                $(target_hw_init_olympus_rc_file) \
-                $(target_hw_ueventd_stingray_rc_file) \
-                $(target_hw_ueventd_olympus_rc_file)
-
-include $(CLEAR_VARS)
-
 COMMON_DIR := vendor/nvidia/common/
 
 ifeq ($(wildcard $(COMMON_DIR)/TegraBoard.mk),$(COMMON_DIR)/TegraBoard.mk)
