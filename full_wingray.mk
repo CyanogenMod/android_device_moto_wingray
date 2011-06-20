7cf72ce0dac93dbdb78b578e49ec3f1334607bcf
Jean-Baptiste Queru <jbq@google.com>
2011-06-20 15:38:46 -0700

    full_wingray is a wingray build, not stingray
    Change-Id: Iae094fdb2884f07fb9c5b218e039f42d761725c0

--- full_wingray.mk
+++ full_wingray.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES := \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, device/moto/stingray/device.mk)
+$(call inherit-product, device/moto/wingray/device.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_wingray
