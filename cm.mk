d33e2590988f084db122eabf926963dd48fed093
Neil Perry <nperry@gmail.com>
2012-01-03 21:21:36 +0000

    wingray: update further $PRODUCT-NAME fixes assetgetprop name & device being same error message my derp my bad
    Change-Id: I562a2fbfb32fc3cf35d200f078b1a2a9d3a67f98

--- cm.mk
+++ cm.mk
@@ -14,4 +14,4 @@ PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Xoom
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wingray BUILD_ID=HTK75D BUILD_DISPLAY_ID=HTK75D BUILD_FINGERPRINT="motorola/tervigon/wingray:3.2.1/HTK75D/190830:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 3.2.1 HTK75D 190830 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=tervigon BUILD_ID=HTK75D BUILD_DISPLAY_ID=HTK75D BUILD_FINGERPRINT="motorola/tervigon/wingray:3.2.1/HTK75D/190830:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 3.2.1 HTK75D 190830 release-keys"
