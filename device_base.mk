b13767a8224ce6c8b65bd9908ffb371e3adcd970
Keun young Park <keunyoung@google.com>
2012-06-07 12:32:04 -0700

    fix for wingray PDK build
    Change-Id: I5f29e7a252d27c41dd62ceb930b791d45be41763

--- device_base.mk
+++ device_base.mk
@@ -60,7 +60,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+    $(call add-to-product-copy-files-if-exists,packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml)
 
 PRODUCT_COPY_FILES += \
         device/moto/wingray/vold.fstab:system/etc/vold.fstab \
