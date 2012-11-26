089babfdd8f32ac53217bc8fb2650cb2325962bf
Takazumi Matsumoto <tdr@taksmon.com>
2012-11-26 02:56:38 +0000

    wingray: Use mini tablet config
    Change-Id: I22aaf923c48f42f702ee324304a939e8feaa76bf

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := XOOM
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/wingray/full_wingray.mk)
